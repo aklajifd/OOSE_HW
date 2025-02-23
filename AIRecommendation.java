75ea498dcfa87020b1eaff043883ccb0301b0d3a
aklajifd <aklajifd@gmail.com>
2025-02-22 21:22:17 -0600

    Fixing indentation

--- AIRecommendation.java
+++ AIRecommendation.java
@@ -1,36 +1,36 @@
 import java.util.Vector;
 
 public class AIRecommendation {
-	private int _iD;
-	private List<Event> _eventHistory;
-	private List<Event> _recommendedEvents;
-	public Vector<Event> _event = new Vector<Event>();
+    private int _iD;
+    private List<Event> _eventHistory;
+    private List<Event> _recommendedEvents;
+    public Vector<Event> _event = new Vector<Event>();
 
-	public List<Event> generateRecommendations(User aUser) {
-		throw new UnsupportedOperationException();
-	}
+    public List<Event> generateRecommendations(User aUser) {
+        throw new UnsupportedOperationException();
+    }
 
-	public int getID() {
-		return this._iD;
-	}
+    public int getID() {
+        return this._iD;
+    }
 
-	public void setID(int aID) {
-		this._iD = aID;
-	}
+    public void setID(int aID) {
+        this._iD = aID;
+    }
 
-	public List<Event> getEventHistory() {
-		throw new UnsupportedOperationException();
-	}
+    public List<Event> getEventHistory() {
+        throw new UnsupportedOperationException();
+    }
 
-	public void setEventHistory(List<Event> aEventHistory) {
-		throw new UnsupportedOperationException();
-	}
+    public void setEventHistory(List<Event> aEventHistory) {
+        throw new UnsupportedOperationException();
+    }
 
-	public List<Event> getRecommendedEvents() {
-		throw new UnsupportedOperationException();
-	}
+    public List<Event> getRecommendedEvents() {
+        throw new UnsupportedOperationException();
+    }
 
-	public void setRecommendedEvents(List<Event> aRecommendedEvents) {
-		throw new UnsupportedOperationException();
-	}
+    public void setRecommendedEvents(List<Event> aRecommendedEvents) {
+        throw new UnsupportedOperationException();
+    }
 }
