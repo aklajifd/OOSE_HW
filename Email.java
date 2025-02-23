6c2d6b1d898ca7bd50fe356d48227a527d8f0e70
aklajifd <aklajifd@gmail.com>
2025-02-22 21:15:23 -0600

    Fixing indentation

--- Email.java
+++ Email.java
@@ -1,6 +1,6 @@
 public class Email {
-    private int _iD;
-    private String _subject;
+	private int _iD;
+	private String _subject;
 	private String _recipient;
 	private String _body;
 	private Date _sentDate;
@@ -10,46 +10,46 @@ public class Email {
 	public Booking _booking;
 
 	public Boolean sendEmail() {
-	throw new UnsupportedOperationException();
+		throw new UnsupportedOperationException();
 	}
 
 	public int getID() {
-	return this._iD;
+		return this._iD;
 	}
 
 	public void setID(int aID) {
-	this._iD = aID;
+		this._iD = aID;
 	}
 
 	public String getSubject() {
-	return this._subject;
+		return this._subject;
 	}
 
 	public void setSubject(String aSubject) {
-	this._subject = aSubject;
+		this._subject = aSubject;
 	}
 
 	public String getRecipient() {
-	return this._recipient;
+		return this._recipient;
 	}
 
 	public void setRecipient(String aRecipient) {
-	this._recipient = aRecipient;
+		this._recipient = aRecipient;
 	}
 
 	public String getBody() {
-	return this._body;
+		return this._body;
 	}
 
 	public void setBody(String aBody) {
-	this._body = aBody;
+		this._body = aBody;
 	}
 
 	public Date getSentDate() {
-	return this._sentDate;
+		return this._sentDate;
 	}
 
 	public void setSentDate(Date aSentDate) {
-	this._sentDate = aSentDate;
+		this._sentDate = aSentDate;
 	}
 }
